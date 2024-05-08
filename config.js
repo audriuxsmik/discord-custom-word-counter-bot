0cfb3b1aad33392a4763bded25d6a4151ceb34b4
audriuxsmik <118130070+audriuxsmik@users.noreply.github.com>
2024-05-08 20:24:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     customWord: "", // the custom word you want to be logged
     activity: "", // discord bot activity
     appid: "", // your bot id
-    guildid: "", // the server id you that you want to count
+    guildid: "", // the server id you that you want to count in
 }
   
   
